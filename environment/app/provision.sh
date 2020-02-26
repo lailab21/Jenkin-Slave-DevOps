cc2251b0c3dd5fad3ac346d0f7b47710be6ec0aa
Laila Barradouane <LBarradouane@spartaglobal.com>
2020-02-26 11:16:08 +0000

    initial commit

---
+++ environment/app/provision.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+# install git
+sudo apt-get install git -y
+
+#install java
+sudo apt install openjdk-8-jdk -y
+
+# install nodejs
+sudo apt-get install python-software-properties -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# install pm2
+sudo npm install pm2 -g
+
+sudo apt-get install nginx -y
+
+# finally, restart the nginx service so the new config takes hold
+sudo service nginx restart
+
+echo "export DB_HOST=mongodb://192.168.10.150:27017/posts" >> /home/vagrant/.bashrc
+
+# copy own ssh public key to vm
+ sudo cat/vagrant/jenkins_key.pub >> /home/vagrant/.ssh/authorized_keys
